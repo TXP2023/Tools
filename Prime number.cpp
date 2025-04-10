3f2d0bc106c0fa6c912d1d9df12a2a5478dc3a44
新沛谭 <bobi20110114@outlook.com>
2025-04-10 19:10:21 +0800

    Prime number

--- Prime number.cpp
+++ Prime number.cpp
@@ -5,8 +5,10 @@ typedef unsigned long long uint64_t;
 
 using namespace std;
 int main() {
+	//对于输入的n， 输出大等于n的最小素数
 	uint64_t n;
 	cin >> n;
+	--n;
 	while (n++) {
 		uint64_t Sqrt = sqrt(n);
 		for (uint64_t i = 2; i <= sqrt(n); ++i) {
