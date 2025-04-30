7f79432dc0fa9c03258febe7f25aa23e4b527cf6
新沛谭 <bobi20110114@outlook.com>
2025-04-30 20:36:44 +0800

    十进制转32位二进制

--- 十进制转32位二进制.py
+++ 十进制转32位二进制.py
@@ -1,6 +1,8 @@
 import crayon
 while True:
     num = int(input())
+    if num == -1:
+        break
     num_32 = num & 0xFFFFFFFF  # 确保处理32位范围内的值
     binary_str = bin(num_32)[2:].zfill(32)  # 转换为二进制并补零到32位
     groups = [binary_str[i:i+4] for i in range(0, 32, 4)]
